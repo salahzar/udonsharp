e0dbe2abd3f25e0bed21e67218ceb74c05562da6
Salahzar Stenvaag <salahzar@gmail.com>
2021-06-18 10:32:37 +0200

    vari schemi grafici

---
+++ 6.meeting16giugno/README.md
@@ -0,0 +1,17 @@
+In questa cartella i seguenti script
+
+* Fai apparire sparire un oggetto:
+![](GraphReveal.jpg)
+[U_ClickReveal.cs UDON#](U_ClickReveal.cs)
+
+* Script di teleport
+  ![](GraphTeleport.jpg)
+  [U_ClickTeleport.cs UDON#](U_ClickTeleport.csReveal.cs)
+
+* Script accendi/spegni musica
+![](GraphPlayMusic.jpg)
+[U_ClickToggleSound.cs](U_ClickToggleSound.cs)
+
+* Script per rotazione porta
+![](GraphRotazione.jpg)
+[U_TriggerRotazione.cs](U_TriggerRotazione.cs)
---
+++ 6.meeting16giugno/U_TriggerRotazione.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
+
+public class U_TriggerRotazione : UdonSharpBehaviour
+{
+    public Transform porta;
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        SendCustomNetworkEvent(NetworkEventTarget.All, "ApriPorta");
+    }
+    public override void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        SendCustomNetworkEvent(NetworkEventTarget.All, "ChiudiPorta");
+    }
+    public void ApriPorta()
+    {
+        porta.transform.rotation = Quaternion.Euler(new Vector3(0, -90, 0));
+    }
+    public void ChiudiPorta()
+    {
+        porta.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+    }
+}
